551996e7c35a560e882d412e81ce058cf672ea39
BowwowBread <bowwow.bread@gmail.com>
2017-11-13 18:10:26 +0900

    test

--- index.py
+++ index.py
@@ -29,6 +29,7 @@ def handle_messages():
   if data["object"] == "page":
       for entry in data["entry"]:
           for messaging_event in entry["messaging"]:
+              print(messaging_event)
               if messaging_event.get("message"):  # someone sent us a message
                   sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                   recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
