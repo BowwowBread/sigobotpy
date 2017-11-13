199fb8b17849dc7003625c649d855ac7e5ee341d
BowwowBread <bowwow.bread@gmail.com>
2017-11-13 17:47:31 +0900

    test

--- index.py
+++ index.py
@@ -40,21 +40,20 @@ def messaging_events(payload):
   messaging_events = data["entry"][0]["messaging"]
   for event in messaging_events:
     if "message" in event and "text" in event["message"]:
-      print("a")
+      print(event)
       yield event["sender"]["id"], event["message"]["text"].encode('unicode_escape')
     else:
-      print("b")
+      print(event)
       yield event["sender"]["id"], "I can't echo this"
 
 
 def send_message(token, recipient, text):
-  message = text.decode('unicode_escape')
   if(message):
     r = requests.post("https://graph.facebook.com/v2.6/me/messages",
       params={"access_token": token},
       data=json.dumps({
         "recipient": {"id": recipient},
-        "message": {"text": message}
+        "message": {"text": text}
       }),
       headers={'Content-type': 'application/json'})
 
