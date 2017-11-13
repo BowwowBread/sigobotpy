9912d0d5fee0fca950944ef0cc7fb8588a1ee2ad
BowwowBread <bowwow.bread@gmail.com>
2017-11-13 18:07:45 +0900

    test

--- index.py
+++ index.py
@@ -59,7 +59,7 @@ def send_message(token, recipient, text):
   """
 
   r = requests.post("https://graph.facebook.com/v2.6/me/messages",
-    params={"access_token": token},
+    params={"access_token": access_token},
     data=json.dumps({
       "recipient": {"id": recipient},
       "message": {"text": text}
