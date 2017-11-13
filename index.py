3024c93637eda7d98de78ef725974eb900f8351b
bowwow <bowwow.bread@gmail.com>
2017-11-14 00:33:48 +0900

    payload

--- index.py
+++ index.py
@@ -45,35 +45,36 @@ def payload_match(sender_id, payload):
   if(payload == "STARTED"):
     send_text(sender_id, "안녕하세요 SIGO 봇입니다. 메뉴에서 도움말 버튼을 누르시면 사용법을 확인하실 수 있습니다.")
   elif(payload == "CAFETERIA"):
-    attachment = {
-      "type": "template",
-      "payload": {
-        "template_type": "button",
-        "text": "날짜를 선택해주세요",
-        "buttons": [
-          {
-            "type": "postback",
-            "title": "오늘 급식",
-            "payload": "TODAY_CAFETERIA"
-          },
-          {
-            "type": "postback",
-            "title": "내일 급식",
-            "payload": "TOMORROW_CAFETERIA"
-          },
-          {
-            "type": "postback",
-            "title": "이번 주 급식",
-            "payload": "WEEK_CAFETERIA"
-          },
-          {
-            "type": "postback",
-            "title": "다음 주 급식",
-            "payload": "NEXTWEEK_CAFETERIA"
-          },
-        ]
+    attachment = json.dumps({
+      {"type": "template",
+        "payload": {
+          "template_type": "button",
+          "text": "날짜를 선택해주세요",
+          "buttons": [
+            {
+              "type": "postback",
+              "title": "오늘 급식",
+              "payload": "TODAY_CAFETERIA"
+            },
+            {
+              "type": "postback",
+              "title": "내일 급식",
+              "payload": "TOMORROW_CAFETERIA"
+            },
+            {
+              "type": "postback",
+              "title": "이번 주 급식",
+              "payload": "WEEK_CAFETERIA"
+            },
+            {
+              "type": "postback",
+              "title": "다음 주 급식",
+              "payload": "NEXTWEEK_CAFETERIA"
+            },
+          ]
+        }
       }
-    }
+    })
     send_buttton(sender_id, attachment)
   elif(payload == "SCHEDULE"):
     print(payload)
