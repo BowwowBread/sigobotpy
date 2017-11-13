f2cafb86147e11a57a879ca5de711bc61e581024
bowwow <bowwow.bread@gmail.com>
2017-11-14 00:55:54 +0900

    payload

--- index.py
+++ index.py
@@ -1,7 +1,6 @@
 from flask import Flask, request
 import json
 import requests
-import bot
 
 app = Flask(__name__)
 
@@ -37,44 +36,15 @@ def handle_messages():
               if messaging_event.get("postback"):
                   sender_id = messaging_event["sender"]["id"]   
                   payload = messaging_event["postback"]["payload"]
-                  payload_match(sender_id, payload)
+                  payload_match(sender_id, message_text, payload)
   return "ok"
 
 
-def payload_match(sender_id, payload):
+def payload_match(sender_id, message_text, payload):
   if(payload == "STARTED"):
     send_text(sender_id, "안녕하세요 SIGO 봇입니다. 메뉴에서 도움말 버튼을 누르시면 사용법을 확인하실 수 있습니다.")
   elif(payload == "CAFETERIA"):
-    # attachment = json.dumps({
-    #   "type": "template",
-    #   "payload": {
-    #     "template_type": "button",
-    #     "text": "날짜를 선택해주세요",
-    #     "buttons": [
-    #       {
-    #         "type": "postback",
-    #         "title": "오늘 급식",
-    #         "payload": "TODAY_CAFETERIA"
-    #       },
-    #       {
-    #         "type": "postback",
-    #         "title": "내일 급식",
-    #         "payload": "TOMORROW_CAFETERIA"
-    #       },
-    #       {
-    #         "type": "postback",
-    #         "title": "이번 주 급식",
-    #         "payload": "WEEK_CAFETERIA"
-    #       },
-    #       {
-    #         "type": "postback",
-    #         "title": "다음 주 급식",
-    #         "payload": "NEXTWEEK_CAFETERIA"
-    #       },
-    #     ]
-    #   }
-    # })
-    # send_buttton(sender_id, att)
+    print(payload)
   elif(payload == "SCHEDULE"):
     print(payload)
   elif(payload == "ENDTOEND"):
@@ -82,17 +52,17 @@ def payload_match(sender_id, payload):
   else:
     print("payload error")
 
-def send_buttton(sender_id, text, title, payload):
-  data = json.dumps({
-    "recipient": {"id": sender_id},
-    "message": {
-      "attachment": {
-        "type": "postback",
-        "text": 
-      }
-    }
-  })
-  send_message(data)
+# def send_buttton(sender_id, message_text, title, payload):
+#   data = json.dumps({
+#     "recipient": {"id": sender_id},
+#     "message": {
+#       "attachment": {
+#         "type": "postback",
+#         "text": 
+#       }
+#     }
+#   })
+#   send_message(data)
   
 def send_text(sender_id, message_text):
   data = json.dumps({
