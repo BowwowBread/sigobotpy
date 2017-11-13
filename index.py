d951a241b3d8a842fff30617acc296f73571c532
bowwow <bowwow.bread@gmail.com>
2017-11-13 23:02:56 +0900

    test

--- index.py
+++ index.py
@@ -37,15 +37,14 @@ def handle_messages():
                   print("postback")
                   pass
   return "ok"
-# def messaging_events(payload):
-#   data = json.loads(payload)
-#   messaging_events = data["entry"][0]["messaging"]
-#   for event in messaging_events:
-#     if "message" in event and "text" in event["message"]:
-#       yield event["sender"]["id"], event["message"]["text"].encode('unicode_escape')
-#     else:
-#       yield event["sender"]["id"], "I can't echo this"
-
+def messaging_events(payload):
+  data = json.loads(payload)
+  messaging_events = data["entry"][0]["messaging"]
+  for event in messaging_events:
+    if "message" in event and "text" in event["message"]:
+      yield event["sender"]["id"], event["message"]["text"].encode('unicode_escape')
+    else:
+      yield event["sender"]["id"], "I can't echo this"
 
 def send_message(token, recipient, text):
   r = requests.post("https://graph.facebook.com/v2.6/me/messages",
