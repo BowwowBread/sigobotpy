45db22e2cd547a47b52cda3a025fc69215382440
BowwowBread <bowwow.bread@gmail.com>
2017-11-13 17:53:43 +0900

    test

--- index.py
+++ index.py
@@ -53,7 +53,7 @@ def send_message(token, recipient, text):
     params={"access_token": token},
     data=json.dumps({
       "recipient": {"id": recipient},
-      "message": {"text": text.decode('unicode_escape')}
+      "message": {"text": text}
     }),
     headers={'Content-type': 'application/json'})
 
