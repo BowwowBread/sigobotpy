5196607c2a64a83857a9ded217c3283d540369b2
bowwow <bowwow.bread@gmail.com>
2017-11-14 00:47:06 +0900

    payload

--- json.py
+++ json.py
@@ -1,35 +1,35 @@
-data = json.dumps({
-  "recipient": {
-    "id": sender_id
-  },
-  "message": {
-    "attachment": {
-      "type": "template",
-      "payload": {
-        "template_type": "button",
-        "text": "날짜를 선택해주세요",
-        "buttons": [{
-            "type": "postback",
-            "title": "오늘 급식",
-            "payload": "TODAY_CAFETERIA"
-          },
-          {
-            "type": "postback",
-            "title": "내일 급식",
-            "payload": "TOMORROW_CAFETERIA"
-          },
-          {
-            "type": "postback",
-            "title": "이번 주 급식",
-            "payload": "WEEK_CAFETERIA"
-          },
-          {
-            "type": "postback",
-            "title": "다음 주 급식",
-            "payload": "NEXTWEEK_CAFETERIA"
-          },
-        ]
-      }
-    }
-  }
-})
+# data = json.dumps({
+#   "recipient": {
+#     "id": sender_id
+#   },
+#   "message": {
+#     "attachment": {
+#       "type": "template",
+#       "payload": {
+#         "template_type": "button",
+#         "text": "날짜를 선택해주세요",
+#         "buttons": [{
+#             "type": "postback",
+#             "title": "오늘 급식",
+#             "payload": "TODAY_CAFETERIA"
+#           },
+#           {
+#             "type": "postback",
+#             "title": "내일 급식",
+#             "payload": "TOMORROW_CAFETERIA"
+#           },
+#           {
+#             "type": "postback",
+#             "title": "이번 주 급식",
+#             "payload": "WEEK_CAFETERIA"
+#           },
+#           {
+#             "type": "postback",
+#             "title": "다음 주 급식",
+#             "payload": "NEXTWEEK_CAFETERIA"
+#           },
+#         ]
+#       }
+#     }
+#   }
+# })
