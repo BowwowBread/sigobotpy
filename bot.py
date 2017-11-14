295ec142de51751d39eb5cc16f779d167f5d1e1d
BowwowBread <bowwow.bread@gmail.com>
2017-11-14 14:12:58 +0900

    package

--- bot.py
+++ bot.py
@@ -35,13 +35,13 @@ def day(day):
             if v.div.contents[0] == str(day):
                 # 해당 날짜 데이터 체크                
                 if len(v.div) != 1 :
-                    result += currentMonth + '월' + str(day) + '일 급식입니다. \n'
+                    result += currentMonth + '월' + str(day) + '일 급식 \n'
                     result += re.sub('[0-9a-zA-Z(\.)<>]','',str(v.div)).replace('/', '\n').replace('[중식]', str(day))
                     return result 
                 else :
-                    return str(day) + "일은 급식을 먹는 날이 아닙니다. "
+                    return str(day) + "일은 급식을 먹는 날이 아니에요"
     if result == "" :
-        return str(day) + "일은 급식을 먹는 날이 아닙니다."
+        return str(day) + "일은 급식을 먹는 날이 아니에요"
 
 def week(state):
     result = ""
@@ -52,32 +52,32 @@ def week(state):
             if v.div.contents[0] == str(currentDay) :
                 # 이번주
                 if state == 1 :
-                    result += "이번 주 급식입니다. \n"
+                    result += "이번 주 급식 \n"
                     timeout = 0
                     daycount = 0
                     for i in v.parent.find_all('div') :
                         if len(i) != 1:
                             result += re.sub('[0-9a-zA-Z(\.)<>]', '', str(i)).replace('/', '\n').replace('[중식]', i.contents[0])
                         elif daycount != 6 and daycount != 0:
-                            result += i.contents[0] + "일은 급식을 먹는날이 아닙니다. \n"
+                            result += i.contents[0] + "일은 급식을 먹는날이 아니에요 \n"
                             timeout += 1                            
                         elif timeout >= 5 :
-                            return "이번 주는 급식을 먹는날이 아닙니다. \n"
+                            return "이번 주는 급식을 먹는날이 아니에요 \n"
                         daycount += 1
                     return result
                 # 다음주
                 elif state == 0 :
-                    result += "다음 주 급식입니다. \n"
+                    result += "다음 주 급식 \n"
                     timeout = 0
                     daycount = 0                    
                     for i in v.parent.find_next_sibling('tr').find_all('div') :
                         if len(i) != 1 :
                             result += re.sub('[0-9a-zA-Z(\.)<>]', '', str(i)).replace('/', '\n').replace('[중식]', i.contents[0])
                         elif len(i) != 1 and daycount != 6 and daycount != 0:
-                            result += i.contents[0] + "일은 급식을 먹는날이 아닙니다. \n"
+                            result += i.contents[0] + "일은 급식을 먹는날이 아니에요 \n"
                             timeout += 1                           
                         elif timeout >= 5 :
-                            return "다음 주는 급식을 먹는날이 아닙니다. \n"
+                            return "다음 주는 급식을 먹는날이 아니에요 \n"
                         daycount += 1
                     return result
 def dayofweek(day):
@@ -92,13 +92,13 @@ def dayofweek(day):
                 # 날짜 찾기    
                 dayday = re.sub('[a-zA-Z(\.)(\n)<>]', '',daylist[day].split('<br/>')[0])
                 if len(daylist[day]) != 17:
-                    result += weekdic[day] + "요일 급식입니다"
+                    result += weekdic[day] + "요일 급식"
                     result += re.sub('[0-9a-zA-Z(\.)<>]', '', daylist[day]).replace('/', '\n').replace('[중식]', dayday)    
                     return result
                 elif dayday != ' ' :
-                    return str(dayday) +"일 " + weekdic[day] + "요일은 급식을 먹는날이 아닙니다."
+                    return str(dayday) +"일 " + weekdic[day] + "요일은 급식을 먹는날이 아니에요"
                 else :
-                    return weekdic[day] + "요일은 급식을 먹는날이 아닙니다."
+                    return weekdic[day] + "요일은 급식을 먹는날이 아니에요"
 
 # print('------------------day test---------------')
 # for i in range(1,32) :
@@ -111,12 +111,23 @@ def dayofweek(day):
 #     print(dayofweek(i))
 
 #--------------test--------------
-
+dayCount = 0
 def messageMatching(message):
+  dayCount = 0
   message = message.replace(' ', '')
   if(textMatching(message, ["급식", "점심", "밥"], 0.3)):
-    if(tomorrowMatching(message, ["내일", "낼"], 0.4)):
+    if(textMatching(message, ["내일", "낼"], 0.4)):
       return day(int(currentDay) + 1)
+    elif(textMatching(message, ["이번주", "요번주"], 0.5)):
+      return week(1)
+    elif(textMatching(message, ["다음주", "담주"], 0.5)):
+      return week(0)
+    elif(textMatching(message, ["월요일, 화요일, 수요일, 목요일, 금요일"], 0.5)):
+      return dayofweek(dayCount)
+    elif(textMatching(message, ["일"], 0.5)):
+      date = re.sub('[^0-9]', '', message)
+      if(int(date) >= 1 and int(date) <= 31):
+        return day(int(date))
     else:
       return day(currentDay)
   else:
@@ -124,13 +135,6 @@ def messageMatching(message):
 
 def textMatching(message, matchText, ratio):
   for text in matchText:
+    dayCount += 1
     if(SequenceMatcher(None, text, message).ratio() > ratio):
       return True
-
-def tomorrowMatching(message, matchText, ratio):
-  for text in matchText:
-    if(SequenceMatcher(None, text, message).ratio() > ratio):
-      return True
-
-
-
