eea6298c5fe6927c0a47e4dce7906410fc0413a1
BowwowBread <bowwow.bread@gmail.com>
2017-11-14 10:53:34 +0900

    get user profile

--- index.py
+++ index.py
@@ -26,6 +26,7 @@ def handle_messages():
   data = request.get_json()
   if data["object"] == "page":
       for entry in data["entry"]:
+          print(entry)
           for messaging_event in entry["messaging"]:
               print(messaging_event)
               if messaging_event.get("message"): 
--- test.py
+++ test.py
@@ -1,8 +1,39 @@
 import json
 
 attachment = {
-  "type": "postback"
-}
+      "type":"template",
+      "payload":{
+        "template_type":"button",
+        "text":"날짜를 선택해주세요.",
+        "buttons":[
+          {
+            "type":"postback",
+            "title":"오늘 급식",
+            "payload":"TODAY_CAFETERIA"
+          },
+          {
+            "type":"postback",
+            "title":"내일 급식",
+            "payload":"TOMORROW_CAFETERIA"
+          },
+          {
+            "type":"postback",
+            "title":"요일 선택",
+            "payload":"DAYOFWEEK_CAFETERIA"
+          },
+          {
+            "type":"postback",
+            "title":"이번 주 급식",
+            "payload":"WEEK_CAFETERIA"
+          },
+          {
+            "type":"postback",
+            "title":"다음 주 급식",
+            "payload":"NEXTWEEK_CAFETERIA"
+          },
+        ]
+      }
+    }
 data = json.dumps({
   "recipient": {"id": "sender_id"},
   "message": {
