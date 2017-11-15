eda248ac922b75db48855fc41f9581434a59afec
BowwowBread <bowwow.bread@gmail.com>
2017-11-15 14:45:41 +0900

    a

--- scheduler.py
+++ scheduler.py
@@ -1,10 +1,23 @@
+import requests
+import index
+import cafeteria
+
 def testing1():
     print ("testing1 - every 5 sec...")
 def testing2():
     print ("testing2 - kick off @ 19:00 hours...")
+
+def postCafeteria():
+  result = cafeteria.day(cafeteria.currentDay)
+  print(result)
+  # r = requests.post("https://graph.facebook.com/v2.8/1529061383780127/feed",
+  #   params={"access_token": index.access_token},
+  #   data=result,
+  #   headers={'Content-type': 'application/json'})
+  
 if __name__ == '__main__':
     from apscheduler.schedulers.blocking import BlockingScheduler
     sched = BlockingScheduler()
-    sched.add_job(testing1, 'cron', id='run_every_2_min', second='*/5')
+    sched.add_job(testing1, 'cron', id='run_every_2_min', minute='*/3')
     sched.add_job(testing2, 'cron', id='run_at_7_pm', hour='19')
 sched.start()
