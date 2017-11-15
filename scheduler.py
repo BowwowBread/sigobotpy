353c19b9422fcb388b6ccd02c1d7be3dafb9b129
BowwowBread <bowwow.bread@gmail.com>
2017-11-15 14:37:47 +0900

    a

--- scheduler.py
+++ scheduler.py
@@ -1,13 +1,10 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
-
-import pytz
-
-sched = BlockingScheduler()
-timezone = pytz.timezone('Asia/Seoul')
-
-@sched.scheduled_job('cron', day_of_week='mon-fri', minute=41)
-def main():
-  print("hi")
-
+def testing1():
+    print ("testing1 - every 5 sec...")
+def testing2():
+    print ("testing2 - kick off @ 19:00 hours...")
+if __name__ == '__main__':
+    from apscheduler.schedulers.blocking import BlockingScheduler
+    sched = BlockingScheduler()
+    sched.add_job(testing1, 'cron', id='run_every_2_min', second='*/5')
+    sched.add_job(testing2, 'cron', id='run_at_7_pm', hour='19')
 sched.start()
-print("aa")
