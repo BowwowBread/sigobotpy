a53440cc6b5cce1695720c652d3e6c5f8bbeb9c7
BowwowBread <bowwow.bread@gmail.com>
2017-11-16 14:17:42 +0900

    scheduler

--- index.py
+++ index.py
@@ -27,7 +27,7 @@ def handle_verification():
 @app.route('/webhook', methods=['POST'])
 def handle_messages():
   try:
-    data = jsonify(request.get_json(force = True))
+    data = request.get_json(force = True)
     if data["object"] == "page":
         for entry in data["entry"]:
             if("messaging" in entry):
