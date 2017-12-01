0bfe1a2a6361df40c6bbfa0da30b728f67128da8
BowwowBread <bowwow.bread@gmail.com>
2017-12-01 11:10:50 +0900

    a

--- index.py
+++ index.py
@@ -57,7 +57,7 @@ def handle_messages():
                       send_text(sender_id, "뭐")
     return "ok", 200
   except:
-    pass
+    return "fail", 403
 
 def payload_match(sender_id, payload):
   if(payload == "STARTED"):
