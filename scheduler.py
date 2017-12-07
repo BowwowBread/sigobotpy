76f0048f4ed6ae16789094d2d628d8317eb2e57f
BowwowBread <bowwow.bread@gmail.com>
2017-12-07 10:42:06 +0900

    sadf

--- scheduler.py
+++ scheduler.py
@@ -4,19 +4,22 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 def postCafeteria():
   cafeteria.timeReset()
-  result = "좋은 아침입니다 \n" + cafeteria.day(cafeteria.currentDay)
-
-  r = requests.post("https://graph.facebook.com/v2.11/1529061383780127/feed",
-    params={"access_token": "EAAHGoGpG0ZCMBAHbsZCP5ZBw89T3c8M2zJavUx0s8ZCbksx7pO0NA6P9nQ0XlWBTZAsKK7VfMhD3kg7NSSDJkYS0ZAssJsZB26UqnlwF27HRAQmffwXy1BOVTUQ5cN4BxvL1GNnSr3AOttCmnjbSVRzSSs6iBkgIN6uFLcCgklUpgZDZD"},
-    data={
-      "message" : result,
-    }
-  )
-  if(r.status_code == "200"):
-    print("success")
-    print(r.text)  
+  result = cafeteria.day(cafeteria.currentDay)
+  if(len(result) <= 20):
+    pass
   else:
-    print(r.text)
+    result = "좋은 아침입니다 \n" + cafeteria.day(cafeteria.currentDay)
+    r = requests.post("https://graph.facebook.com/v2.11/1529061383780127/feed",
+      params={"access_token": "EAAHGoGpG0ZCMBAHbsZCP5ZBw89T3c8M2zJavUx0s8ZCbksx7pO0NA6P9nQ0XlWBTZAsKK7VfMhD3kg7NSSDJkYS0ZAssJsZB26UqnlwF27HRAQmffwXy1BOVTUQ5cN4BxvL1GNnSr3AOttCmnjbSVRzSSs6iBkgIN6uFLcCgklUpgZDZD"},
+      data={
+        "message" : result,
+      }
+    )
+    if(r.status_code == "200"):
+      print("success")
+      print(r.text)  
+    else:
+      print(r.text)
 
 sched = BlockingScheduler()
 sched.add_job(postCafeteria, 'cron', hour=10)
