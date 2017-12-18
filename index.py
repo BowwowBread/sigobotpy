0632c236c95efd22015afdb49f990e4a841f6c61
BowwowBread <bowwow.bread@gmail.com>
2017-12-18 10:53:35 +0900

    Asdf

--- index.py
+++ index.py
@@ -126,33 +126,5 @@ def send_message(data):
     data=data,
     headers={'Content-type': 'application/json'})
 
-
-def postCafeteria():
-  cafeteria.timeReset()
-  result = cafeteria.day(cafeteria.currentDay)
-  if(len(result) <= 20):
-    pass
-  else:
-    result = "좋은 아침입니다 \n" + cafeteria.day(cafeteria.currentDay)
-    r = requests.post("https://graph.facebook.com/v2.11/1529061383780127/feed",
-      params={"access_token": "EAAHGoGpG0ZCMBAHbsZCP5ZBw89T3c8M2zJavUx0s8ZCbksx7pO0NA6P9nQ0XlWBTZAsKK7VfMhD3kg7NSSDJkYS0ZAssJsZB26UqnlwF27HRAQmffwXy1BOVTUQ5cN4BxvL1GNnSr3AOttCmnjbSVRzSSs6iBkgIN6uFLcCgklUpgZDZD"},
-      data={
-        "message" : result,
-      }
-    )
-    if(r.status_code == "200"):
-      print("success")
-      print(r.text)  
-    else:
-      print(r.text)
-      
-def test():
-  print("test")
-scheduler = BackgroundScheduler()
-scheduler.start()
-scheduler.add_job(postCafeteria, 'cron', hour=10)
-
-atexit.register(lambda: scheduler.shutdown())
-
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=80, debug = False)
